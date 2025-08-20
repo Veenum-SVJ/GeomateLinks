5bee80fb053c797d600398882efad82b9bb17b25
Haarith Adam <adhaarith@gmail.com>
2025-08-20 22:25:18 +0000

    make the components in the nav bar of the CMS dashboard be at the center

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -35,7 +35,7 @@ export default function DashboardLayout({
   return (
     <div className="flex min-h-screen w-full flex-col">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-50">
-        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <nav className="hidden flex-1 flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:justify-center md:gap-5 md:text-sm lg:gap-6">
           {navItems.map((item) => {
             const isActive = pathname === item.href;
             return (
@@ -53,7 +53,7 @@ export default function DashboardLayout({
             );
           })}
         </nav>
-        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <div className="flex items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
             <div className='ml-auto'>
                 <DropdownMenu>
                   <DropdownMenuTrigger asChild>
