7161f2f9ee48cd5ae9252adb4069515832abcfd8
Veenum <veenum@example.com>
2026-09-02 20:57:15 +0100

    fix: remove nested BrowserRouter causing React crash

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
